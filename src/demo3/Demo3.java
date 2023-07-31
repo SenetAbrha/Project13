0c9828cbda98a9d8507c04ada8d51dfb62cca7af
SenetAbrha <140857441+SenetAbrha@users.noreply.github.com>
2023-07-30 20:39:04 -0400

    Update Demo3.java

--- src/demo3/Demo3.java
+++ src/demo3/Demo3.java
@@ -3,7 +3,13 @@ package demo3;
 public class Demo3 {
 	
 	public static void main(String[] args) {
-		System.out.println("heyp");
+		System.out.println("heyo");
+	}
+	//hello from git hub
+	public static void fruits(String sweet, String sour){
+               sweet="apple";
+		sour="lemon";
+		
 	}
 
 }
